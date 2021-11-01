0af3d155e316c513677b5368444ba9c8b5a13e21
Naoyuki Hayasaka <lelivredes5anneaux@gmail.com>
2021-11-01 22:40:06 +0900

    Completed the React-Router issue

---
+++ src/pages/History.tsx
@@ -0,0 +1,16 @@
+import { VFC } from "react";
+
+const History: VFC = () => {
+  return (
+    <div>
+      <h1>経歴</h1>
+      <ul>
+        <li>◯◯年生まれ</li>
+        <li>2015~2018年 株式会社A 営業</li>
+        <li>2018年~現在 株式会社B フロントエンド開発</li>
+      </ul>
+    </div>
+  );
+};
+
+export default History;
---
+++ src/pages/Work.tsx
@@ -0,0 +1,24 @@
+import { VFC } from "react";
+import { useParams } from "react-router-dom";
+import works, { Data } from "../data/works";
+
+type ID = {
+  id: string;
+};
+
+const Work: VFC = () => {
+  const { id } = useParams<ID>();
+  const data: Data = works[Number(id)];
+
+  return (
+    <div>
+      <a href="/works">実績</a> ＞ {data.id}
+      <h1>{data.name}</h1>
+      <a target="blank" href={data.url}>
+        {data.url}
+      </a>
+    </div>
+  );
+};
+
+export default Work;
